e35c432a9390edf11beeeba2902e7c04cb20ab93
Roman Broshnitskiy <romario777v8@gmail.com>
2022-07-09 22:10:10 +0300

    add auth redux

---
+++ src/redux/authorization/auth-selectors.js
@@ -0,0 +1,5 @@
+export const getIsLoggedIn = state => state.auth.isLoggedIn;
+
+export const getUserEmail = state => state.auth.user.email;
+
+export const getIsFetchingCurrent = state => state.auth.isFetchingCurrentUser;
