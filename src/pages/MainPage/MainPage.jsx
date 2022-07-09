bdb71ede72818cbe6d775a93f1a7d4f307d39310
Roman Broshnitskiy <romario777v8@gmail.com>
2022-07-09 13:08:32 +0300

    create MainPaige template

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,17 @@
+// import React, { useState } from 'react';
 import { Container } from '../../components/container';
+import { BgImageStyled } from './MainPage.styled';
 
 export const MainPage = () => {
+  // const [modalData, setModalData] = useState({});
+  // const [isOpenModal, setIsOpenModal] = useState(false);
+
   return (
     <main>
+      <BgImageStyled />
       <Container>
-        <div>MainPage</div>
+        {/* <DailyCaloriesForm />
+        <Modal /> */}
       </Container>
     </main>
   );
