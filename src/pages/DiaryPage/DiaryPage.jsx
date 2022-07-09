4f501ebdf28f46a079b9638a3621b0c8d2980299
Roman Broshnitskiy <romario777v8@gmail.com>
2022-07-10 00:44:11 +0300

    styled calendar

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -1,19 +1,38 @@
 import Datetime from 'react-datetime';
 import 'react-datetime/css/react-datetime.css';
 import { GoCalendar } from 'react-icons/go';
+import { useState } from 'react';
+import { format } from 'date-fns';
+import { CalendarStyles } from './DiaryPage.styled';
 
 export const DiaryPage = () => {
+  const [date, setDate] = useState(new Date());
+  const dateReformat = format(date, 'dd/MM/yyyy');
+  console.log(dateReformat);
+
+  const isCurrentDay =
+    format(date, 'dd/MM/yyyy') === format(new Date(), 'dd/MM/yyyy');
+
+  console.log(isCurrentDay);
   return (
     <main>
-      <Datetime
-        dateFormat="DD.MM.YYYY"
-        timeFormat={false}
-        initialValue={new Date()}
-        onChange={data => {
-          console.log(data._d);
-        }}
-      />
-      <GoCalendar size={50} fill={'red'} />
+      <CalendarStyles>
+        <Datetime
+          dateFormat="DD.MM.YYYY"
+          timeFormat={false}
+          initialValue={new Date()}
+          inputProps={{
+            className: 'calendar',
+            placeholder: format(new Date(), 'dd.MM.yyyy'),
+          }}
+          onChange={data => {
+            data._d && setDate(data._d);
+          }}
+          closeOnSelect
+        />
+        <GoCalendar size={20} fill={'#9B9FAA'} className="calendar_icon" />
+      </CalendarStyles>
+
       <div>DiaryPage</div>
     </main>
   );
---
+++ src/pages/DiaryPage/DiaryPage.styled.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const CalendarStyles = styled.div`
+  .calendar {
+    font-family: 'Verdana';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 22px;
+    border: none;
+    width: 150px;
+    text-align: center;
+
+    ::placeholder {
+      color: black;
+    }
+  }
+`;
