d92e3df43b875e096bee24ac622cb51575e714ad
YaroslavOnofriichuk <YaroslavOnofriichuk@ukr.net>
2022-07-10 20:36:11 +0300

    DailyCaloriesForm

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 // import React, { useState } from 'react';
 import { Container } from '../../components/container';
 import { BgImageStyled } from './MainPage.styled';
+import { DailyCaloriesForm } from '../../components/dailyCaloriesForm/DailyCaloriesForm';
 
 export const MainPage = () => {
   // const [modalData, setModalData] = useState({});
@@ -10,8 +11,8 @@ export const MainPage = () => {
     <main>
       <BgImageStyled />
       <Container>
-        {/* <DailyCaloriesForm />
-        <Modal /> */}
+        <DailyCaloriesForm />
+        {/* <Modal /> */}
       </Container>
     </main>
   );
