0a0589d36d6ba33c3a863e02ea92c6725739c755
Taras-Roboteckyi <rtvr@ukr.net>
2022-07-10 15:26:40 +0300

    add SideBar markup

--- src/pages/DiaryPage/DiaryPage.jsx
+++ src/pages/DiaryPage/DiaryPage.jsx
@@ -1,7 +1,10 @@
+import SideBar from '../../components/SideBar/SideBar';
+
 export const DiaryPage = () => {
   return (
     <main>
       <div>DiaryPage</div>
+      <SideBar />
     </main>
   );
 };
