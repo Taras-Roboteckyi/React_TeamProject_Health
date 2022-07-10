6062dd5a2889215d4dfb875edb5d28de444007cb
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2022-07-10 19:41:37 +0300

    background for LoginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,15 @@
 import { LoginForm } from "../../components/LoginForm/LoginForm";
-
+import { ImageStyled } from "../LoginPage/LoginPage.styled";
+import { Container } from "../../components/container";
 
 export const LoginPage = () => {
   return (
     <main>
-      <h2>SING IN</h2>
-      <LoginForm />
+      <ImageStyled />
+      <Container>
+        <h2>SING IN</h2>
+        <LoginForm />
+      </Container>  
     </main>
   );
 };
