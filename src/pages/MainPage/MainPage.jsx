e0a127630cbb135712af3550a26ea1e61adf56b1
YaroslavOnofriichuk <YaroslavOnofriichuk@ukr.net>
2022-07-11 18:43:45 +0300

    add DailyCalorieIntake

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,18 +1,33 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { Container } from '../../components/container';
 import { BgImageStyled } from './MainPage.styled';
 import { DailyCaloriesForm } from '../../components/DailyCaloriesForm/DailyCaloriesForm';
+import { DailyCalorieIntake } from '../../components/DailyCalorieIntake/DailyCalorieIntake';
+import Modal from '../../components/modal/Modal';
 
 export const MainPage = () => {
-  // const [modalData, setModalData] = useState({});
-  // const [isOpenModal, setIsOpenModal] = useState(false);
+  const [modalData, setModalData] = useState({});
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const onFormSubmit = data => {
+    const exampleData = {
+      calories: 2800,
+      products: ['Flour products', 'Milk', 'Red meat', 'Smoked meats'],
+    };
+    setModalData(exampleData);
+    setIsOpenModal(true);
+  };
 
   return (
     <main>
       <BgImageStyled />
       <Container>
-        <DailyCaloriesForm />
-        {/* <Modal /> */}
+        <DailyCaloriesForm onFormSubmit={onFormSubmit} />
+        {isOpenModal && (
+          <Modal>
+            <DailyCalorieIntake data={modalData} />
+          </Modal>
+        )}
       </Container>
     </main>
   );
