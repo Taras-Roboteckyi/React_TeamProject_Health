d763a0ca45d1ddc6a7904a411d0fd0e9b7973451
Taras-Roboteckyi <rtvr@ukr.net>
2022-07-11 13:36:31 +0300

    fix auth

--- src/redux/authorization/auth-selectors.js
+++ src/redux/authorization/auth-selectors.js
@@ -1,5 +1,5 @@
 export const getIsLoggedIn = state => state.auth.isLoggedIn;
 
-export const getUserEmail = state => state.auth.user.email;
+export const getUserName = state => state.auth.user.name;
 
 export const getIsFetchingCurrent = state => state.auth.isFetchingCurrentUser;
