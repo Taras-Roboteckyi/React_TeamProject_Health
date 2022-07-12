812e05dd803deea068f762ca6c43409634e2d6eb
YaroslavOnofriichuk <YaroslavOnofriichuk@ukr.net>
2022-07-12 13:02:08 +0300

    add RegistrationForm

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,22 +1,57 @@
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Section } from './RegistrationForm.Styled';
+import { useDispatch } from 'react-redux';
+import authOperations from '../../redux/authorization/auth-operations';
 
 export const RegistrationForm = ({ onFormSubmit }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onLoginClick = () => {
+    navigate('/signin');
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .min(3, 'Too short!')
+      .max(254, 'Too long!')
+      .required('This field is required!'),
+    email: Yup.string()
+      .email('Not a proper email')
+      .min(3, 'Too short!')
+      .max(254, 'Too long!')
+      .required('This field is required!'),
+    password: Yup.string()
+      .matches(
+        /^(?=.*[a-z])(?=.*[0-9])(?!.*[^a-zA-Z0-9])/,
+        'Must contain at least one Latin letter and one number, without special case characters',
+      )
+      .min(8, 'Too short!')
+      .max(100, 'Too long!')
+      .required('This field is required!'),
+  });
+
   const formik = useFormik({
     initialValues: {
       name: '',
       email: '',
       password: '',
     },
-    // validationSchema,
+    validationSchema,
     onSubmit: values => {
-      onFormSubmit(values);
+      // onFormSubmit(values);
+      dispatch(authOperations.register({ values }));
     },
   });
 
   return (
-    <>
-      <form onSubmit={formik.handleSubmit}>
+    <Section>
+      <h3 className="form-title">Register</h3>
+
+      <form onSubmit={formik.handleSubmit} className="form">
         <input
           name="name"
           className="form-input"
@@ -35,7 +70,7 @@ export const RegistrationForm = ({ onFormSubmit }) => {
           name="email"
           className="form-input"
           placeholder="Email *"
-          type="text"
+          type="email"
           autoComplete="off"
           onChange={formik.handleChange}
           onBlur={formik.handleBlur('email')}
@@ -49,7 +84,7 @@ export const RegistrationForm = ({ onFormSubmit }) => {
           name="password"
           className="form-input"
           placeholder="Password *"
-          type="text"
+          type="password"
           autoComplete="off"
           onChange={formik.handleChange}
           onBlur={formik.handleBlur('password')}
@@ -59,9 +94,16 @@ export const RegistrationForm = ({ onFormSubmit }) => {
           <p className="form-input-error">{formik.errors.password}</p>
         ) : null}
 
-        <button type="submit">Register</button>
+        <div className="buttons">
+          <button className="login-button" type="button" onClick={onLoginClick}>
+            Login
+          </button>
+          <button className="register-button" type="submit">
+            Register
+          </button>
+        </div>
       </form>
-    </>
+    </Section>
   );
 };
 
