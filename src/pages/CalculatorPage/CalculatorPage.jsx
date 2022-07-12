ab8d4c57ebe6e443dc5144f31abcbae148cd5c99
Iryna Hrynko <ir.hrynko@gmail.com>
2022-07-12 22:01:38 +0200

    add Sidebar

--- src/pages/CalculatorPage/CalculatorPage.jsx
+++ src/pages/CalculatorPage/CalculatorPage.jsx
@@ -1,10 +1,16 @@
 import { DailyCaloriesForm } from "../../components/DailyCaloriesForm/DailyCaloriesForm";
+import SideBar from '../../components/SideBar/SideBar';
 import {StyledWrapper} from './CalculatorPage.styled'
 
 export const CalculatorPage = () => {
   return (
+    <> 
    <StyledWrapper>
-      <DailyCaloriesForm/>
-    </StyledWrapper>
+      <DailyCaloriesForm />
+      
+      </StyledWrapper>
+        <SideBar />
+
+      </>
   );
 };
