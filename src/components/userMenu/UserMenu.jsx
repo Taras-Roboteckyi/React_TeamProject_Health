5922e100003716c086ae2e9db29dd67bbce0346e
Taras-Roboteckyi <rtvr@ukr.net>
2022-07-13 13:41:47 +0300

    fix burger image

--- src/components/userMenu/UserMenu.jsx
+++ src/components/userMenu/UserMenu.jsx
@@ -7,7 +7,7 @@ import {
 } from './UserMenu.styled';
 
 import AuthNavLogined from '../AuthNav/AuthNavLogined';
-import BurgerIcon from '../../images/BurgerIcon.svg';
+/* import BurgerIcon from '../../images/BurgerIcon.svg'; */
 import { useWindowWidth } from '@react-hook/window-size';
 
 // import { TbArrowBack } from 'react-icons/tb';
@@ -27,14 +27,14 @@ export default function UserMenu() {
         </ContainerArrowStyled>
       )} */}
 
-      {windowWidth <= 767 && <img src={BurgerIcon} alt="Меню" width={18} />}
+      {/* {windowWidth <= 767 && <img src={BurgerIcon} alt="Меню" width={18} />} */}
       <ContainerTextStyled>
         {/* <p>{ userName }</p> */}
         <p>Роман</p>
         <button onClick={() => dispatch(authOperations.logOut())}>Вихід</button>
         {windowWidth >= 768 && (
           <button onClick={() => dispatch()}>
-            {windowWidth <= 1279 && <img src={BurgerIcon} alt="Меню" />}
+            {/* {windowWidth <= 1279 && <img src={BurgerIcon} alt="Меню" />} */}
           </button>
         )}
       </ContainerTextStyled>
