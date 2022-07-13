e5dea95b59e5b2376bee623c16480a4ff9096a51
Roman Broshnitskiy <romario777v8@gmail.com>
2022-07-13 18:00:13 +0300

    add user login name

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -4,19 +4,28 @@ import Navigation from '../Navigation/Navigation';
 import UserMenu from '../userMenu/UserMenu';
 import AuthNav from '../AuthNav/AuthNav';
 import { authSelectors } from '../../redux/authorization';
-
+import AuthNavLogined from '../AuthNav/AuthNavLogined';
 import { Header, Wrapper } from './AppBar.styled';
 import { Container } from '../container';
+import { useWindowWidth } from '@react-hook/window-size';
 
 export default function AppBar() {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  const windowWidth = useWindowWidth();
 
   return (
     <Header>
       <Container>
         <Wrapper>
           <Navigation />
-          {isLoggedIn /* true */ ? <UserMenu /> : <AuthNav />}
+          {isLoggedIn ? (
+            <>
+              {windowWidth > 1280 && <AuthNavLogined />}
+              <UserMenu />
+            </>
+          ) : (
+            <AuthNav />
+          )}
         </Wrapper>
       </Container>
     </Header>
